e7b647c4c7eb67623097ceb1bd0f69c5661bbf8e
Ethan MacLeod <ethandavidfrancis@gmail.com>
2025-06-14 12:10:09 +1200

    Add contact me to navbar

--- src/lib/components/nav/left-nav.svelte
+++ src/lib/components/nav/left-nav.svelte
@@ -5,11 +5,18 @@
 <aside class="w-44 border border-solid border-gray-600 bg-[#ffffcb] p-2">
 	<nav>
 		<ul class="flex flex-col gap-2">
-			<li class="my-2"><a href="/" class="underline">Home</a></li>
-			<hr class="border-gray-600" />
+			<a
+				href="/"
+				class="border-3 border-t-gray-200 border-r-gray-500 border-b-gray-500 border-l-gray-300 bg-[#d4d8d5]"
+				><img
+					src="https://images.cooltext.com/5732587.gif"
+					class="h-[40px] w-full"
+					alt="Home Page"
+				/></a
+			>
 			<GifButton href="/about" gifName="about-me" alt="About Me" />
 			<GifButton href="/projects" gifName="projects" alt="Projects" />
-			<li class="my-2"><a href="/contact" class="underline">Contact</a></li>
+			<GifButton href="/contact" gifName="contact-me" alt="Contact Me" />
 		</ul>
 	</nav>
 </aside>
