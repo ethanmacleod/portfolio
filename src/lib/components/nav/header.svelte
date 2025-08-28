7327600f1b4531e18292f1afc0601df3b1a1dc27
Ethan MacLeod <ethandavidfrancis@gmail.com>
2025-08-28 18:54:51 +1200

    feat: consolidate widgets

--- src/lib/components/nav/header.svelte
+++ src/lib/components/nav/header.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
-	import TotalViewsWidget from '../TotalViewsWidget.svelte';
-	import BadActorsWidget from '../BadActorsWidget.svelte';
-	
+	import HeaderWidget from '../HeaderWidget.svelte';
+
 	interface Analytics {
 		totalViews: number;
 		badActors: number;
 	}
-	
+
 	export let analytics: Analytics;
 </script>
 
-<header class="flex items-center justify-between bg-black gap-4 p-2">
-	<TotalViewsWidget count={analytics.totalViews} />
-	<BadActorsWidget count={analytics.badActors} />
+<header class="flex gap-4 bg-black p-2">
+	<HeaderWidget
+		title="Welcome!"
+		text="You are visitor #{analytics.totalViews}!"
+		icon="/gifs/globe.gif"
+		iconAlt="Globe Icon"
+		textColor="text-blue-600"
+	/>
+	<HeaderWidget
+		title="Bad Actors Blocked!"
+		text="{analytics.badActors} bots/scrapers blocked"
+		icon="/gifs/globe.gif"
+		iconAlt="Warning Icon"
+		textColor="text-red-600"
+	/>
 </header>
